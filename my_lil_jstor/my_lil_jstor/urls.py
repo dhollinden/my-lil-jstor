36099cc4b98eea0333c4035d219896a5fb66d702
Evan Slawski <evan.slawski@ithaka.org>
2019-02-13 09:38:44 -0500

    Update url pattern for django 2

--- my_lil_jstor/my_lil_jstor/urls.py
+++ my_lil_jstor/my_lil_jstor/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     url(r'^coloringbook/(?P<book_id>[0-9]+)', views.coloring_books, name='coloring_books'),
     url(r'^purchase/(?P<book_id>[0-9]+)', views.purchase, name='purchase'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^', views.home, name='home'),
 ]
